c90cfaaaa65ee6542241871e4ea4766d8fd8eb28
Flex01a <91494774+Flex01a@users.noreply.github.com>
2023-07-18 21:40:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
-const say = require('say');
+const { exec } = require('child_process');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
@@ -13,19 +13,35 @@ app.post('/generate-voice', upload.single('audio'), async (req, res) => {
   const audioFilePath = req.file.path;
 
   try {
-    // Copy voice from the uploaded audio (using the 'say' package)
-    say.export(text, null, 0.5, audioFilePath, async (err) => {
-      if (err) {
-        console.error('Error copying voice:', err);
+    const outputFilePath = `generated/${Date.now()}_output.wav`;
+
+    // Use ffmpeg to lower the volume of the original audio
+    const ffmpegCommand = `ffmpeg -i ${audioFilePath} -filter:a "volume=0.5" ${outputFilePath}`;
+
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Error copying voice:', error);
         res.status(500).send('An error occurred while copying voice.');
         return;
       }
 
-      // Send the generated audio file back to the client
-      res.sendFile(audioFilePath);
+      // Use ffmpeg to add text-to-speech voice to the audio
+      const textToSpeechCommand = `ffmpeg -i ${outputFilePath} -vf "drawtext=text='${text}':fontsize=24:fontcolor=white:x=10:y=10" ${outputFilePath}`;
 
-      // Clean up the uploaded file
-      fs.unlinkSync(audioFilePath);
+      exec(textToSpeechCommand, (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error generating voice:', error);
+          res.status(500).send('An error occurred while generating voice.');
+          return;
+        }
+
+        // Send the generated audio file back to the client
+        res.sendFile(outputFilePath);
+
+        // Clean up uploaded and generated files
+        fs.unlinkSync(audioFilePath);
+        fs.unlinkSync(outputFilePath);
+      });
     });
   } catch (error) {
     console.error('Error generating voice:', error);
