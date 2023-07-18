c1810131001ef116229dfb3e3d73fa29c38a7050
Flex01a <91494774+Flex01a@users.noreply.github.com>
2023-07-18 21:39:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 const express = require('express');
 const multer = require('multer');
-const { exec } = require('child_process');
+const fs = require('fs');
+const say = require('say');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 app.use(express.static('public'));
 
-app.post('/generate-voice', upload.single('audio'), (req, res) => {
-    const { text } = req.body;
-    const audioFilePath = req.file.path;
-    const outputFilePath = `generated/${Date.now()}_output.wav`;
+app.post('/generate-voice', upload.single('audio'), async (req, res) => {
+  const { text } = req.body;
+  const audioFilePath = req.file.path;
 
-    const command = `ffmpeg -i ${audioFilePath} -filter:a "volume=0.5" ${outputFilePath}`;
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error('Error copying voice:', error);
-            res.status(500).send('An error occurred while copying voice.');
-            return;
-        }
+  try {
+    // Copy voice from the uploaded audio (using the 'say' package)
+    say.export(text, null, 0.5, audioFilePath, async (err) => {
+      if (err) {
+        console.error('Error copying voice:', err);
+        res.status(500).send('An error occurred while copying voice.');
+        return;
+      }
 
-        const textToSpeechCommand = `espeak-ng -w ${outputFilePath} "${text}"`;
-        exec(textToSpeechCommand, (error, stdout, stderr) => {
-            if (error) {
-                console.error('Error generating voice:', error);
-                res.status(500).send('An error occurred while generating voice.');
-                return;
-            }
+      // Send the generated audio file back to the client
+      res.sendFile(audioFilePath);
 
-            const audioFile = fs.readFileSync(outputFilePath);
-            res.contentType('audio/wav');
-            res.send(audioFile);
-
-            // Clean up generated files
-            fs.unlinkSync(audioFilePath);
-            fs.unlinkSync(outputFilePath);
-        });
+      // Clean up the uploaded file
+      fs.unlinkSync(audioFilePath);
     });
+  } catch (error) {
+    console.error('Error generating voice:', error);
+    res.status(500).send('An error occurred while generating voice.');
+  }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
